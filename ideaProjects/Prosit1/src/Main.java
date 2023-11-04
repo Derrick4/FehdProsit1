eada8ded555364761b84db1b2bdfef1d845f0b9b
Derrick4 <81701774+Derrick4@users.noreply.github.com>
2023-11-04 18:42:28 +0100

    Update Main.java Prosit2

--- ideaProjects/Prosit1/src/Main.java
+++ ideaProjects/Prosit1/src/Main.java
@@ -1,22 +1,39 @@
-import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        // Create a zoo with a maximum of 5 cages
+        zoo myZoo = new zoo("My Zoo", "My City", 5);
 
-        Scanner scanner = new Scanner(System.in);
+        // Create animals with a constructor
+        Animal lion = new Animal("Feline", "Lion", 5, true);
+        Animal tiger = new Animal("Feline", "Tiger", 4, true);
+        Animal elephant = new Animal("Mammal", "Elephant", 10, true);
 
-        System.out.print("Entrez le nombre de cages : ");
-        while (!scanner.hasNextInt()) {
-            System.out.println("Veuillez entrer un nombre valide.");
-            scanner.next();
+        // Add animals to the zoo
+        myZoo.addAnimal(lion);
+        myZoo.addAnimal(tiger);
+        myZoo.addAnimal(elephant);
+
+        // Display the animals in the zoo
+        myZoo.displayAnimals();
+
+        // Search for an animal
+        Animal searchAnimal = new Animal("Feline", "Tiger", 4, true);
+        int foundIndex = myZoo.searchAnimal(searchAnimal);
+
+        if (foundIndex != -1) {
+            System.out.println("Animal found at index: " + foundIndex);
+        } else {
+            System.out.println("Animal not found.");
         }
-        int nbrCages = scanner.nextInt();
 
-        System.out.print("Entrez le nom du zoo : ");
-        String zooName = scanner.next();
+        // Remove an animal
+        Animal removeAnimal = new Animal("Feline", "Lion", 5, true);
+        boolean removed = myZoo.removeAnimal(removeAnimal);
 
-        ZooManagement myZoo = new ZooManagement(nbrCages, zooName);
-        myZoo.displayZooInfo();
-
-        scanner.close();
+        if (removed) {
+            System.out.println("Animal removed successfully.");
+        } else {
+            System.out.println("Animal not found or removal failed.");
+        }
     }
 }
